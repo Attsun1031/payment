5ff27f3771823f50c6e4225768bf8e1101bdc586
Attsun1031 <atsumitatsuya@progress.local>
2013-02-01 21:04:30 +0900

    add test case

--- spec/integration/run_add_employee_command_spec.rb
+++ spec/integration/run_add_employee_command_spec.rb
@@ -10,6 +10,31 @@ describe CommandRunner do
       emp.address.should == "埼玉県熊谷市"
       emp.salary_type.should == SalaryType::HOURLY
       emp.salary_unit.should == 1000
+      emp.commissioned.should == nil
+    end
+  end
+
+  context "add monthly rate employee" do
+    it "should add employee" do
+      result = CommandRunner.run ["AddEmp", "田口", "埼玉県熊谷市", "S", 200000]
+      result.should == 0
+      emp = Employee.find_by_name("田口")
+      emp.address.should == "埼玉県熊谷市"
+      emp.salary_type.should == SalaryType::MONTHLY
+      emp.salary_unit.should == 200000
+      emp.commissioned.should == nil
+    end
+  end
+
+  context "add commissioned employee" do
+    it "should add employee" do
+      result = CommandRunner.run ["AddEmp", "田口", "埼玉県熊谷市", "C", 200000, 100000]
+      result.should == 0
+      emp = Employee.find_by_name("田口")
+      emp.address.should == "埼玉県熊谷市"
+      emp.salary_type.should == SalaryType::MONTHLY
+      emp.salary_unit.should == 200000
+      emp.commissioned.should == 100000
     end
   end
 end
